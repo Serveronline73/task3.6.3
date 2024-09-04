7641a9fe7f8e67e9f18836f5b284536e2a6ca789
dennis <info.dennis.durmus@gmail.com>
2024-09-04 14:39:43 +0200

    Aufgabe3 erledigt

--- main.dart
+++ main.dart
@@ -1,6 +1,24 @@
 import "hello_world.dart";
+import "guten_tag.dart";
+import "person.dart";
 
 void main() {
   HeLLoWorld hello = HeLLoWorld();
   hello.sayHello();
+
+  // Aufgabe 2
+
+  GutenTag goodDay = GutenTag("Dennis");
+  goodDay.printGoodDay();
+  goodDay.setName("Volkan");
+  goodDay.printGoodDay();
+
+  // Aufgabe 3
+
+  Person person = Person("Dennis", 51, "Hochtstr. 6");
+  person.printdataBasePerson();
+  person.setName("Volkan");
+  person.setAge(48);
+  person.setnewAddress("Schulstr. 7");
+  person.printdataBasePerson();
 }
